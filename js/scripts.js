d98ebfb249caecaa12db4159aaa24e3b8b69bf59
CarlosBarberan <cbarberan@itsqmet.edu.ec>
2025-01-12 22:25:16 -0500

    1.1

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('mouseenter', () => {
+      anime({
+        targets: button,
+        scale: 1.1,
+        duration: 300,
+        easing: 'easeInOutQuad'
+      });
+    });
+    button.addEventListener('mouseleave', () => {
+      anime({
+        targets: button,
+        scale: 1,
+        duration: 300,
+        easing: 'easeInOutQuad'
+      });
+    });
+  });
+  document.addEventListener('DOMContentLoaded', () => {
+    anime({
+      targets: '.caja',
+      translateY: [-50, 0],
+      opacity: [0, 1],
+      duration: 1000,
+      easing: 'easeOutExpo',
+      delay: anime.stagger(200) // Retrasa la animación de cada elemento
+    });
+  });
+  
